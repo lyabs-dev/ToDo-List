8a17feab92abf68535a49b4af1e9886716fa84bd
yourimukaz <yourikatombmukaz@gmail.com>
2022-11-09 20:53:49 +0200

    add path files

--- lib/utils/my_material.dart
+++ lib/utils/my_material.dart
@@ -5,22 +5,37 @@ export 'package:flutter_localizations/flutter_localizations.dart';
 export 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 //components
+export 'package:flutter_structure/presentation/components/app_button.dart';
+export 'package:flutter_structure/presentation/components/my_text.dart';
+export 'package:flutter_structure/presentation/components/scaffold_color.dart';
 
 //utils
 export 'package:flutter_structure/utils/path_assets.dart';
 export 'package:flutter_structure/utils/constants.dart';
-export 'package:flutter_structure/utils/size_config.dart';
 export 'package:flutter_structure/utils/enums.dart';
+export 'package:flutter_structure/utils/sizes_config.dart';
 
 //data
+export 'package:flutter_structure/data/models/settings_item.dart';
+export 'package:flutter_structure/data/repositories/settings_repository.dart';
 export 'package:flutter_structure/data/models/settings_item.dart';
 export 'package:flutter_structure/data/repositories/settings_repository.dart';
 
 //logic
 export 'package:flutter_structure/logic/cubits/settings_cubit.dart';
 export 'package:flutter_structure/logic/states/settings_state.dart';
+export 'package:flutter_structure/logic/cubits/app_cubit.dart';
+export 'package:flutter_structure/logic/cubits/settings_cubit.dart';
+export 'package:flutter_structure/logic/states/app_state.dart';
+export 'package:flutter_structure/logic/states/settings_state.dart';
 
 //presentation
 export 'package:flutter_structure/presentation/router/app_router.dart';
 export 'package:flutter_structure/presentation/styles/styles.dart';
-export 'package:flutter_structure/presentation/screens/home/home.dart';
+export 'package:flutter_structure/presentation/screens/welcome/welcome.dart';
+export 'package:flutter_structure/presentation/router/app_router.dart';
+export 'package:flutter_structure/presentation/styles/styles.dart';
+export 'package:flutter_structure/presentation/screens/sign_in/sign_in.dart';
+
+
+
