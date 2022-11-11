f3117bc2bd8ad9f57279ef3a19bd241e276e7bfd
yourimukaz <yourikatombmukaz@gmail.com>
2022-11-11 16:05:46 +0200

    add path images

--- lib/utils/path_assets.dart
+++ lib/utils/path_assets.dart
@@ -3,6 +3,7 @@ class PathImage{
   static String signIn = "assets/images/sign_in.png";
   static String signUp = "assets/images/sign_up.png";
   static String home = "assets/images/home.png";
+  static String user = "assets/images/user.png";
 }
 
 class PathIcons{
@@ -11,6 +12,8 @@ class PathIcons{
   static String question = "assets/icons/question.png";
   static String warning = "assets/icons/warning.png";
   static String info = "assets/icons/info.png";
+  static String calendar = "assets/icons/calendar.png";
+  static String graph = "assets/icons/graph.png";
 }
 
 class PathAnimation{
