f8a82f162f310c907a47b33042efd49c9639c193
yourimukaz <yourikatombmukaz@gmail.com>
2022-11-11 16:05:31 +0200

    create home page

---
+++ lib/presentation/screens/home/home.dart
@@ -0,0 +1,141 @@
+import 'package:flutter_structure/presentation/screens/home/components/notes.dart';
+
+import '../../../utils/my_material.dart';
+
+class Home extends StatelessWidget {
+   Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) { List<Note> listNotes = [
+    Note(true, "", "Pay credit card bills amount \$450", "23 juil"),
+    Note(false, "Grocery list", "Milk\nBread\nVeggies\nEgg\nFruits\nShakes", "25 dec"),
+    Note(false, "Figma", "Deliver files export save in drive Egg Fruits Shakes Deliver files export save in drive Egg Fruits Shakes Deliver files export save in drive Egg Fruits Shakes Deliver files export save in drive Egg Fruits Shakes Deliver files export save in drive Egg Fruits Shakes Deliver files export save in drive Egg Fruits Shakes", "2 sept"),
+    Note(false, "Reply mails", "Mails \nFruits \nShakes", "25 dec"),
+  ];
+
+  return Scaffold(
+      body: Stack(
+        children: [
+          SafeArea(
+            child: Container(
+              color: colorWhite,
+
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(textSizeLargeMedium),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        MyText(AppLocalizations.of(context)!.goodMorning,
+                            style: TextStyle(
+                                color: buttonColor,
+                                fontWeight: FontWeight.w700,
+                                fontSize: getProportionateScreenWidth(
+                                    textSizeLarge, context))),
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              image: DecorationImage(
+                                  image: AssetImage(PathImage.user),
+                                  filterQuality: FilterQuality.high,
+                                  fit: BoxFit.cover)),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(textSizeLargeMedium),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Image(
+                              image: AssetImage(PathIcons.calendar),
+                              filterQuality: FilterQuality.high,
+                              height: 25,
+                            ),
+                            SizedBox(
+                              width: getShortSize(10, context),
+                            ),
+                            Card(
+                              elevation: 5,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                              ),
+                              child: AppButton(
+                                "22 Nov",
+                                () {},
+                                primaryColor: colorWhite,
+                                textColor: colorPrimary,
+                                borderRadius: 20,
+                                paddingVertical: 5,
+                              ),
+                            ),
+                          ],
+                        ),
+                        AppButton(
+                          "15 Notes",
+                          () {},
+                          whitIcons: true,
+                          paddingVertical: 7,
+                          borderRadius: 20,
+                          fontSize: textSizeSMedium,
+                          icon: Image(
+                            image: AssetImage(PathIcons.graph),
+                            height: 15,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: getShortSize(10, context),
+                  ),
+                  Expanded(
+                    child: CustomScrollView(
+                      slivers: [
+                        SliverList(
+                          delegate: SliverChildListDelegate([]),
+                        ),
+                        SliverGrid.count(
+                          childAspectRatio:
+                              getProportionateScreenWidth(5.2, context) /
+                                  getProportionateScreenWidth(7, context),
+                          children: List.generate(
+                            listNotes.length,
+                            (index) => Notes(reminder: listNotes[index].reminder,title:listNotes[index].title ,tacks:listNotes[index].tacks ,date:listNotes[index].date ,),
+                          ),
+                          crossAxisCount: 2,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: Image(
+              image: AssetImage(PathImage.home),
+              filterQuality: FilterQuality.high,
+              fit: BoxFit.fitHeight,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Note{
+   bool reminder;
+   String title, tacks, date;
+
+   Note(this.reminder, this.title, this.tacks, this.date);
+}
