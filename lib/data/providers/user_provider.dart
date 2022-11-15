f4da5cfd62187e29a69c487b3e9acf3199f63463
jonathan kalonga <jokalmwamba@gmail.com>
2022-11-15 16:08:44 +0200

    move function in class

--- lib/data/providers/user_provider.dart
+++ lib/data/providers/user_provider.dart
@@ -35,62 +35,64 @@ class UserProvider {
     }
     return hasUpdate;
   }
-}
 
-Future<Map> get(String documentId) async {
-  Map map = {};
+  Future<Map> get(String documentId) async {
+    Map map = {};
 
-  try {
-    var result = await FirebaseFirestore.instance
-        .collection(COLLECTION_USER)
-        .doc(documentId)
-        .get();
-    if (result.data() != null) {
-      map = result.data()!;
-      map[FIELD_USER_DOCUMENT] = result;
-    }
-  } catch (err) {
-    debugPrint('=======Failed to get user:$err');
-  }
-  return map;
-}
-
-Future<Map> getFromAuthId(String authId) async {
-  Map map = {};
-
-  try {
-    var result = await FirebaseFirestore.instance
-        .collection(COLLECTION_USER)
-        .where(FIELD_USER_AUTH_ID, isEqualTo: authId)
-        .get();
-    if (result.docs.isNotEmpty) {
-      map = result.docs.first.data();
-      map[FIELD_USER_DOCUMENT] = result.docs.first;
-    }
-  } catch (err) {
-    debugPrint('===========Failed to get user from auth ID : $err');
-  }
-  return map;
-}
-
-Future<Map<String, Object>> getCurrentUser() async {
-  Map<String, Object> result = {};
-
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  String? userString = prefs.getString(PREF_CURRENT_USER);
-
-  if (userString != null) {
     try {
-      result = json.decode(userString);
+      var result = await FirebaseFirestore.instance
+          .collection(COLLECTION_USER)
+          .doc(documentId)
+          .get();
+      if (result.data() != null) {
+        map = result.data()!;
+        map[FIELD_USER_DOCUMENT] = result;
+      }
     } catch (err) {
-      debugPrint("============Failed to get current user: $err");
+      debugPrint('=======Failed to get user:$err');
     }
+    return map;
   }
-  return result;
+
+  Future<Map> getFromAuthId(String authId) async {
+    Map map = {};
+
+    try {
+      var result = await FirebaseFirestore.instance
+          .collection(COLLECTION_USER)
+          .where(FIELD_USER_AUTH_ID, isEqualTo: authId)
+          .get();
+      if (result.docs.isNotEmpty) {
+        map = result.docs.first.data();
+        map[FIELD_USER_DOCUMENT] = result.docs.first;
+      }
+    } catch (err) {
+      debugPrint('===========Failed to get user from auth ID : $err');
+    }
+    return map;
+  }
+
+  Future<Map<String, Object>> getCurrentUser() async {
+    Map<String, Object> result = {};
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? userString = prefs.getString(PREF_CURRENT_USER);
+
+    if (userString != null) {
+      try {
+        result = json.decode(userString);
+      } catch (err) {
+        debugPrint("============Failed to get current user: $err");
+      }
+    }
+    return result;
+  }
+
+  Future setCurrentUser(Map<String, Object> map) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString(PREF_CURRENT_USER, json.encode(map));
+  }
+
 }
 
-Future setCurrentUser(Map<String, Object> map) async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  await prefs.setString(PREF_CURRENT_USER, json.encode(map));
-}
 
