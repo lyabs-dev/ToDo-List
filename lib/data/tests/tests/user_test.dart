9e9981c86965f33ead94617d5e9b09b69b2e1d74
jonathan kalonga <jokalmwamba@gmail.com>
2022-11-15 16:37:49 +0200

    create user test and test the add user

---
+++ lib/data/tests/tests/user_test.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../../models/user_item.dart';
+import '../../repositories/user_repository.dart';
+import 'package:test/test.dart' as test;
+
+
+class UserTest {
+  static UserItem user = UserItem(
+    authId: '',
+    name: 'jonathan',
+    email: 'jokalmwamba@gmail.com'
+  );
+
+  static UserRepository repository = UserRepository();
+
+  static run() {
+    print('=========Start User Test=========');
+
+    _add();
+    //_update();
+    //_getUser();
+    //_getFromAuthId();
+    //_getCurrentUser();
+  }
+
+  static _add() {
+    test.test('Add', () async {
+      DocumentSnapshot? document = await repository.add(user);
+      user.document = document;
+      test.expect(document, test.isNot(null));
+    });
+  }
+
+
+}
