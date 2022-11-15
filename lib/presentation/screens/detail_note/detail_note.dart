6d74e2597c82380f7273823ea8b55789d5c22103
yourimukaz <yourikatombmukaz@gmail.com>
2022-11-15 11:12:34 +0200

    create detail_note

--- lib/presentation/screens/detail_note/detail_note.dart
+++ lib/presentation/screens/detail_note/detail_note.dart
@@ -1,15 +1,74 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:intl/intl.dart';
 import '../../../utils/my_material.dart';
 
-class DetailNote extends StatelessWidget {
+class DetailNote extends StatefulWidget {
   DetailNote({Key? key}) : super(key: key);
+
+  @override
+  State<DetailNote> createState() => _DetailNoteState();
+}
+
+class _DetailNoteState extends State<DetailNote> {
+  bool _infoNodeTitle = false;
+  bool _infoNodeDescription = false;
+
+  FocusNode _focusTitle = new FocusNode();
+  FocusNode _focusDescription = new FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _focusTitle.addListener(_handleFocusChangeUsename);
+    _focusDescription.addListener(_handleFocusChangeEmail);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _focusTitle.removeListener(_handleFocusChangeUsename);
+    _focusDescription.removeListener(_handleFocusChangeEmail);
+
+    _focusTitle.dispose();
+    _focusDescription.dispose();
+  }
+
+  void _handleFocusChangeUsename() {
+    if (_focusTitle.hasFocus != _infoNodeTitle) {
+      setState(() {
+        _infoNodeTitle = _focusTitle.hasFocus;
+      });
+    }
+  }
+
+  void _handleFocusChangeEmail() {
+    if (_focusDescription.hasFocus != _infoNodeDescription) {
+      setState(() {
+        _infoNodeDescription = _focusDescription.hasFocus;
+      });
+    }
+  }
+
   late TextEditingController titleController;
   late TextEditingController textEditingController;
 
+  DateTime _dateTime = DateTime.now();
+  bool dateChange = false;
+  DateTime _time = DateTime.now();
+  bool timeChange = false;
+
+  TimeOfDay time =
+      TimeOfDay(hour: TimeOfDay.now().hour, minute: TimeOfDay.now().minute);
+
+  String formatDate(DateTime date) => new DateFormat("MMMd").format(date);
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     textEditingController = TextEditingController();
     titleController = TextEditingController();
-    Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: colorWhite,
       body: SafeArea(
@@ -40,24 +99,36 @@ class DetailNote extends StatelessWidget {
                             fontWeight: FontWeight.w600,
                             fontSize: getProportionateScreenWidth(
                                 textSizeNormal, context))),
-                    PopupMenuButton<MenuItem>(
-                      elevation: 1.0,
-                      splashRadius: 20.0,
-                      position: PopupMenuPosition.over,
-                      child:  Image(
-                        image: AssetImage(PathIcons.moreCircle),
-                        filterQuality: FilterQuality.high,
-                        height: 20,
-                      ),
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
-                        itemBuilder: (context) => [
-                              PopupMenuItem(
-                                  value: MenuItem.item1, child: Text("Delete"))
-                            ]),
-                    /* Icon(
-                      Icons.check,
-                      color: colorPrimary,
-                    ),*/
+                    (_infoNodeTitle || _infoNodeDescription)
+                        ? InkWell(
+                            onTap: () {
+                              _focusTitle.unfocus();
+                              _focusDescription.unfocus();
+                            },
+                            child: Icon(
+                              Icons.check,
+                              color: colorPrimary,
+                            ),
+                          )
+                        : PopupMenuButton<MenuItem>(
+                            elevation: 1.0,
+                            splashRadius: 20.0,
+                            position: PopupMenuPosition.over,
+                            child: Image(
+                              image: AssetImage(PathIcons.moreCircle),
+                              filterQuality: FilterQuality.high,
+                              height: 20,
+                            ),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20.0)),
+                            itemBuilder: (context) => [
+                                  PopupMenuItem(
+                                      value: MenuItem.item1,
+                                      child: Text(
+                                        AppLocalizations.of(context)!.delete,
+                                        style: TextStyle(color: colorPrimary, fontSize: getProportionateScreenWidth(textSizeSMedium, context)),
+                                      ))
+                                ]),
                   ],
                 ),
               ),
@@ -73,6 +144,7 @@ class DetailNote extends StatelessWidget {
                         child: AppEditText(
                           "Title",
                           titleController,
+                          focusNode: _focusTitle,
                           hintStyle: TextStyle(
                               color: colorPrimary,
                               fontSize: getProportionateScreenWidth(
@@ -86,13 +158,112 @@ class DetailNote extends StatelessWidget {
                         ),
                       ),
                       IconButton(
-                          onPressed: () {},
+                          onPressed: () async {
+                            if (defaultTargetPlatform == TargetPlatform.iOS) {
+                              showCupertinoModalPopup(
+                                  context: context,
+                                  builder: (context) => CupertinoActionSheet(
+                                        actions: [
+                                          SizedBox(
+                                            height: 150,
+                                            child: CupertinoDatePicker(
+                                              initialDateTime: _dateTime,
+                                              mode:
+                                                  CupertinoDatePickerMode.date,
+                                              onDateTimeChanged: (dateTime) {
+                                                setState(() {
+                                                  _dateTime = dateTime;
+                                                });
+                                              },
+                                            ),
+                                          )
+                                        ],
+                                        cancelButton:
+                                            CupertinoActionSheetAction(
+                                          child: MyText(
+                                            AppLocalizations.of(context)!.done,
+                                            style: TextStyle(
+                                              color: colorPrimary,
+                                            ),
+                                          ),
+                                          onPressed: () {
+                                            setState(() {
+                                              dateChange = !dateChange;
+                                            });
+
+                                            Navigator.of(context).pop();
+                                          },
+                                        ),
+                                      ));
+                            } else {
+                              DateTime? _newDate = await showDatePicker(
+                                  context: context,
+                                  initialDate: _dateTime,
+                                  firstDate: DateTime(1800),
+                                  lastDate: DateTime(3000));
+                              if (_newDate != null) {
+                                setState(() {
+                                  dateChange = !dateChange;
+                                  _dateTime = _newDate;
+                                });
+                              }
+                            }
+                          },
                           icon: Icon(
                             Icons.calendar_month,
                             color: colorPrimary,
                           )),
                       IconButton(
-                          onPressed: () {},
+                          onPressed: () async {
+                            if (defaultTargetPlatform == TargetPlatform.iOS) {
+                              showCupertinoModalPopup(
+                                  context: context,
+                                  builder: (context) => CupertinoActionSheet(
+                                        actions: [
+                                          SizedBox(
+                                            height: 150,
+                                            child: CupertinoDatePicker(
+                                              initialDateTime: _time,
+                                              mode:
+                                                  CupertinoDatePickerMode.time,
+                                              use24hFormat: true,
+                                              onDateTimeChanged: (timePicker) {
+                                                setState(() {
+                                                  _time = timePicker;
+                                                });
+                                              },
+                                            ),
+                                          )
+                                        ],
+                                        cancelButton:
+                                            CupertinoActionSheetAction(
+                                          child: MyText(
+                                            AppLocalizations.of(context)!.done,
+                                            style: TextStyle(
+                                              color: colorPrimary,
+                                            ),
+                                          ),
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                            timeChange = !timeChange;
+                                            //platformIOS = true;
+                                          },
+                                        ),
+                                      ));
+                            } else {
+                              TimeOfDay? _newTime = await showTimePicker(
+                                context: context,
+                                initialTime: time,
+                              );
+                              if (_newTime != null) {
+                                setState(() {
+                                  // platformIOS = false;
+                                  timeChange = !timeChange;
+                                  time = _newTime;
+                                });
+                              }
+                            }
+                          },
                           icon: Icon(
                             Icons.notifications_none,
                             color: colorPrimary,
@@ -103,29 +274,43 @@ class DetailNote extends StatelessWidget {
                       child: AppEditText(
                     "Description",
                     textEditingController,
+                    focusNode: _focusDescription,
                     style: TextStyle(color: colorPrimary),
                     hintStyle: TextStyle(color: colorPrimary),
                     maxLines: 100,
-                  ))
+                  )),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      dateChange
+                          ? AppButton(
+                              "${formatDate(_dateTime)}",
+                              () {},
+                              primaryColor: colorPrimary,
+                              textColor: colorWhite,
+                              fontSize: textSizeSMedium,
+                              borderRadius: 15,
+                              paddingVertical: 4,
+                            )
+                          : SizedBox.shrink(),
+                      SizedBox(
+                        width: getShortSize(10, context),
+                      ),
+                      timeChange
+                          ? AppButton(
+                              "${time.hour}:${time.minute}",
+                              () {},
+                              primaryColor: colorPrimary,
+                              textColor: colorWhite,
+                              fontSize: textSizeSMedium,
+                              borderRadius: 15,
+                              paddingVertical: 4,
+                            )
+                          : SizedBox.shrink()
+                    ],
+                  )
                 ],
               ),
-              /* Align(
-                alignment: Alignment.bottomRight,
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  child: Align(
-                      alignment: Alignment.bottomRight,
-                      child: CircleButton(
-                        Icons.mode_edit_outline_outlined,
-                        () {
-                          // Navigator.of(context).pushNamed(pageDetailNote);
-                        },
-                        iconColor: colorWhite,
-                        secondReaction: colorPrimary,
-                        secondColor: buttonColor,
-                      )),
-                ),
-              )*/
             ],
           ),
         ),
