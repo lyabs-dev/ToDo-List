c4284fa97e3906c47d1221533fcbed9c99b7184a
jonathan kalonga <jokalmwamba@gmail.com>
2022-11-17 15:28:35 +0200

    add crud test

--- lib/data/tests/tests/user_test.dart
+++ lib/data/tests/tests/user_test.dart
@@ -17,10 +17,10 @@ class UserTest {
   static run() {
     print('=========Start User Test=========');
 
-    _add();
+    //_add();
     //_update();
-    //_getUser();
-    //_getFromAuthId();
+    _getUser();
+    _getFromAuthId();
     //_getCurrentUser();
   }
 
@@ -31,6 +31,43 @@ class UserTest {
       print('###########print document##############');
       print(document);
       test.expect(document, test.isNot(null));
+    });
+  }
+
+  static _update() {
+    test.test('Update user', () async {
+      user.name = 'jonnathan442';
+      bool res = await repository.update(user, user.authId);
+      test.expect(res, true);
+    });
+  }
+
+  static _getUser() {
+    test.test('Get user', () async {
+      UserItem? user = await repository.get(UserTest.user.authId);
+      test.expect(user, test.isNot(null));
+    });
+  }
+
+  static _getFromAuthId() {
+    test.test('Get user from auth ID', () async {
+      UserItem? user = await repository.getFromAuthId(UserTest.user.authId);
+
+      if (user != null) {
+        UserTest.user = user;
+      }
+      test.expect(user, test.isNot(null));
+    });
+  }
+
+  static _getCurrentUser(){
+    test.test('Get user current ', ()async{
+      await repository.setCurrentUser(UserTest.user);
+      UserItem? user = await repository.getCurrentUser();
+      if(user != null){
+        UserTest.user = user;
+      }
+      test.expect(user, test.isNot(null));
     });
   }
 
