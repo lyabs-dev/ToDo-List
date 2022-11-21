4e4c2bc09c3f15b4dfb63c5a2ef5dd5b0fa5e88c
jonathan kalonga <jokalmwamba@gmail.com>
2022-11-21 16:29:41 +0200

    update project

--- lib/presentation/screens/sign_in/sign_in.dart
+++ lib/presentation/screens/sign_in/sign_in.dart
@@ -1,3 +1,7 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../logic/cubits/sign_in_cubit.dart';
+import '../../../logic/states/sign_in_state.dart';
 import '../../../utils/my_material.dart';
 import 'components/fields_sign_in.dart';
 
@@ -7,47 +11,6 @@ class SignIn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Scaffold(
-        backgroundColor: colorWhite,
-        body: Stack(
-          children: [
-            SafeArea(
-              child: SingleChildScrollView(
-                child: Container(
-                  color: colorWhite,
-                  width: size.width,
-                  height: size.height,
-                  padding: EdgeInsets.only(
-                      left: paddingLargeMedium,
-                      right: paddingLargeMedium,
-                      bottom: paddingXXXLarge),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        height: getShortSize(50, context),
-                      ),
-                      MyText(AppLocalizations.of(context)!.welcomeBack,
-                          style: TextStyle(
-                              color: colorPrimary,
-                              fontFamily: poppins,
-                              fontSize: getProportionateScreenWidth(
-                                  textSizeXXLarge, context),
-                              fontWeight: FontWeight.w900)),
-                      FieldSignIn(),
-                      FooterPage()
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Align(
-              alignment: Alignment.topCenter,
-              child: Image(image: AssetImage(PathImage.signIn),filterQuality: FilterQuality.high,fit: BoxFit.cover,),
-            ),
-          ],
-        ));
     return BlocProvider<SignInCubit>(
         create: (context) => SignInCubit(SignInState()),
         child: BlocBuilder<SignInCubit,SignInState>(
