5a7ec41d5a712c0ce89e809b3689c0c1408e6f50
jonathan kalonga <jokalmwamba@gmail.com>
2022-11-22 23:26:08 +0200

    implement bloc provider

--- lib/presentation/screens/sign_up/sign_up.dart
+++ lib/presentation/screens/sign_up/sign_up.dart
@@ -1,3 +1,7 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_structure/logic/cubits/sign_up_cubit.dart';
+import 'package:flutter_structure/logic/states/sign_up_state.dart';
+
 import '../../../utils/my_material.dart';
 import 'components/field_sign_up.dart';
 
@@ -7,51 +11,71 @@ class SignUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: Stack(
-        children: [
-          SafeArea(
-            child: SingleChildScrollView(
-              child: Container(
-                width: size.width,
-                height: size.height,
-                color: colorPrimary,
-                padding: EdgeInsets.only(
-                    left: paddingLargeMedium,
-                    right: paddingLargeMedium,
-                    bottom: paddingXXXLarge),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
+    return BlocProvider<SignUpCubit>(
+      create: (context) => SignUpCubit(SignUpState()),
+      child: BlocListener<SignUpCubit,SignUpState>(
+        listener: (context,state){
+          if(state.responseCode != null) {
+            if(state.responseCode?.code == SignUpCode.Connected) {
+              Navigator.of(context).pushNamed(pageHome);
+            }
+          }
+        },
+        child: BlocBuilder<SignUpCubit,SignUpState>(
+            builder: (context,state) {
+              return Scaffold(
+                body: Stack(
                   children: [
-                    SizedBox(
-                      height: getShortSize(50, context),
+                    SafeArea(
+                      child: SingleChildScrollView(
+                        child: Container(
+                          width: size.width,
+                          height: size.height,
+                          color: colorPrimary,
+                          padding: EdgeInsets.only(
+                              left: paddingLargeMedium,
+                              right: paddingLargeMedium,
+                              bottom: paddingXXXLarge),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                height: getShortSize(50, context),
+                              ),
+                              MyText(AppLocalizations.of(context)!.createAccount,
+                                  style: TextStyle(
+                                      color: colorWhite,
+                                      fontFamily: poppins,
+                                      fontSize: getProportionateScreenWidth(
+                                          paddingXXLarge, context),
+                                      fontWeight: FontWeight.w900)),
+                              FieldSignUp(
+                                nameEditingController: state.usernameEditingController,
+                                emailEditingController: state.emailEditingController,
+                                passwordEditingController: state.passwordEditingController,
+                              ),
+                              FooterPage(signIn: context.read<SignUpCubit>().signUp(),
+                                signUp: true,
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
-                    MyText(AppLocalizations.of(context)!.createAccount,
-                        style: TextStyle(
-                            color: colorWhite,
-                            fontFamily: poppins,
-                            fontSize: getProportionateScreenWidth(
-                                paddingXXLarge, context),
-                            fontWeight: FontWeight.w900)),
-                    FieldSignUp(),
-                    FooterPage(
-                      signUp: true,
-                    )
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: Image(
+                        image: AssetImage(PathImage.signUp),
+                        filterQuality: FilterQuality.high,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ],
                 ),
-              ),
-            ),
-          ),
-          Align(
-            alignment: Alignment.topCenter,
-            child: Image(
-              image: AssetImage(PathImage.signUp),
-              filterQuality: FilterQuality.high,
-              fit: BoxFit.cover,
-            ),
-          ),
-        ],
+              );
+            }
+        ),
       ),
     );
   }
