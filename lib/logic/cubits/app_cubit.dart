2ee67683db80d2dcc8d212e33c1ad127fb61061c
jonathan kalonga <jokalmwamba@gmail.com>
2022-11-24 11:58:14 +0200

    manage user and save in shared prefferences

--- lib/logic/cubits/app_cubit.dart
+++ lib/logic/cubits/app_cubit.dart
@@ -3,7 +3,12 @@ import 'package:flutter_structure/data/tests/app_test.dart';
 import 'package:flutter_structure/logic/states/app_state.dart';
 import 'package:flutter_structure/utils/enums.dart';
 
+import '../../data/models/user_item.dart';
+import '../../data/repositories/user_repository.dart';
+
 class AppCubit extends Cubit<AppState> {
+
+  UserRepository userRepository = UserRepository();
 
   AppCubit(AppState initialState) : super(initialState) {
     intiData();
@@ -14,8 +19,21 @@ class AppCubit extends Cubit<AppState> {
     state.loadingState = CustomState.loading;
     emit(state.copy());
 
+    state.user = await userRepository.getCurrentUser();
+    //emit(state.copy());
+
+    print('************PRINT USER#################');
+    print(state.user);
+
     state.loadingState = CustomState.done;
     emit(state.copy());
   }
+
+  Future setUser(UserItem user) async {
+    state.user = user;
+    await userRepository.setCurrentUser(user);
+    emit(state.copy());
+  }
+
 
 }
