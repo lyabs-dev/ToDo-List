a76e271c3db92a11292657325f53ef8577393c1e
jonathan kalonga <jokalmwamba@gmail.com>
2022-11-28 11:20:15 +0200

    get data and intialisate it for home

--- lib/logic/cubits/home_cubit.dart
+++ lib/logic/cubits/home_cubit.dart
@@ -1,12 +1,53 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_structure/data/models/note_item.dart';
 import 'package:flutter_structure/logic/states/home_state.dart';
 
 import '../../data/repositories/note_repository.dart';
+import '../../utils/enums.dart';
 
-class HomeCubit extends Cubit{
+class HomeCubit extends Cubit<HomeState>{
 
   NoteRepository noteRepository = NoteRepository();
 
-  HomeCubit(HomeState initialState): super(initialState);
+  HomeCubit(HomeState initialState): super(initialState){
+    intiData();
+  }
+
+
+  intiData() async {
+    state.loadingState = true;
+    emit(state.copy());
+
+
+    HomeCode? code = await _gettingNotes();
+    state.loadingState = false;
+    
+    print("**************####THE CODE $code");
+
+    if(code != null) {
+      state.responseCode = HomeResponse(code: code, messageType: MessageType.toast) ;
+      
+    }
+
+    emit(state.copy());
+  }
+
+  Future<HomeCode?> _gettingNotes() async {
+
+    try{
+      List<NoteItem?> notes = await noteRepository.getNotes();
+      if(notes.isEmpty)  {
+        return HomeCode.EmptyNote;
+      }
+
+      state.notes = notes;
+      state.noteNumber = state.notes.length;
+      return HomeCode.Success;
+
+    } on FirebaseException catch (e) {
+      return HomeCode.Error;
+    }
+  }
 
 }
