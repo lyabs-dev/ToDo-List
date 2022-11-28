55024e4dc3193fc8a7ebc229905aedd547a40823
jonathan kalonga <jokalmwamba@gmail.com>
2022-11-28 11:19:13 +0200

    add some variable to home state

--- lib/logic/states/home_state.dart
+++ lib/logic/states/home_state.dart
@@ -1,17 +1,69 @@
 import '../../data/models/note_item.dart';
+import '../../data/models/response_code_item.dart';
 import '../../utils/my_material.dart';
 
 class HomeState {
-  TextEditingController TitleEditingController = TextEditingController(),DescriptionEditingController = TextEditingController();
-  List<NoteItem> notes;
-  int? noteNumber;
+  List<NoteItem?> notes;
+  int noteNumber;
   NoteItem? currentNote;
+  DateTime? sortingDate;
+  HomeResponse? responseCode;
+  bool loadingState;
 
-  HomeState({this.noteNumber ,this.notes = const [], this.currentNote});
+
+  HomeState({this.loadingState= true,this.responseCode,this.sortingDate,this.noteNumber = 0 ,this.notes = const [], this.currentNote});
 
   HomeState copy() {
-    HomeState copy = HomeState(noteNumber:noteNumber, notes:notes);
+    HomeState copy = HomeState(
+        loadingState: loadingState,
+        responseCode: responseCode,
+        currentNote: currentNote,
+        sortingDate: sortingDate,
+        noteNumber:noteNumber,
+        notes:notes);
 
     return copy;
   }
+
+}
+
+enum HomeCode {
+  Error,
+  EmptyNote,
+  Success
+}
+
+class HomeResponse extends ResponseCodeItem {
+
+  @override
+  MessageType messageType;
+
+  HomeCode code;
+
+  HomeResponse({required this.code, this.messageType: MessageType.toast});
+  @override
+  String message(BuildContext context) {
+    switch (code) {
+      case HomeCode.EmptyNote:
+        return AppLocalizations.of(context)!.emptyNote;
+      case HomeCode.Error:
+        return AppLocalizations.of(context)!.error;
+      case HomeCode.Success:
+        return AppLocalizations.of(context)!.success;
+    }
+  }
+
+  @override
+  // TODO: implement type
+  DialogType get type {
+    switch(code) {
+      case HomeCode.Error:
+        return DialogType.error;
+      case HomeCode.EmptyNote:
+        return DialogType.info;
+      case HomeCode.Success:
+        return DialogType.success;
+    }
+  }
+
 }
