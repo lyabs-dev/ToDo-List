fe78de23430b42a30080d85440c0330c093c4c0c
jonathan kalonga <jokalmwamba@gmail.com>
2022-11-28 12:15:04 +0200

    put name in parametre

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,7 @@ class MyApp extends StatelessWidget {
                   }
                   if(appState.user != null){
 
-                    return Home();
+                    return Home(appState.user!);
                   }
                   return const WelcomePage();
                 },
